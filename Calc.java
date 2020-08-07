40c49c6f4b215dfbe02d9165992f5ea5968e25f7
rishita27 <55452507+rishita27@users.noreply.github.com>
2020-08-07 18:05:00 +0530

    Update Calc.java

--- Calc.java
+++ Calc.java
@@ -15,6 +15,6 @@ class Calc{
     System.out.println(add(10,20));
     System.out.println(sub(10,20));
     System.out.println(mul(10,20));
-    System.out.println(div(10,20));                       
+   // System.out.println(div(10,20));                       
   }
 }
